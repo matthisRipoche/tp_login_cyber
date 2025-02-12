5992d76e9588fde3f75fe6b4fbffaae5c7905379
matthisRipoche <matthisripoche@gmail.com>
2025-02-12 14:01:20 +0100

    start

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Sécurisé</title>
+</head>
+
+<body>
+    <h1>Formulaire Login</h1>
+    <form action="" method="post">
+
+    </form>
+</body>
+
+</html>
