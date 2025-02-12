365049078ce8fb12f55a82e17e9cfa6598ff472e
matthisRipoche <matthisripoche@gmail.com>
2025-02-12 14:02:22 +0100

    form

--- index.php
+++ index.php
@@ -10,7 +10,11 @@
 <body>
     <h1>Formulaire Login</h1>
     <form action="" method="post">
-
+        <label for="login">Login</label>
+        <input type="text" name="login" id="login" required>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required>
+        <input type="submit" value="Login">
     </form>
 </body>
 
